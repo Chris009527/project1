edae55a31bdb36e926bbe7ce34ace44afdc11faf
陳柏勳 <a0926233329@gmail.com>
2019-01-17 15:17:45 +0800

    version9 ds3project3.cpp

--- ds3project3.cpp
+++ ds3project3.cpp
@@ -1,7 +1,8 @@
- class Student{
+  class Student{
     public:
         void makeMove(int Record[5][6], int Max[5][6], Color color[5][6], Color inputColor){
-            bool bblue = (inputColor||White||Black);
+            Color player1;
+
             if(inputColor == Red){
                 player1 =  Blue;
             }else{
@@ -9,82 +10,124 @@
             }
 
 
-
-            for(int k=0;k<30;k++){
-                for(int i=4;i>=0;i--){//up
-                    for(int j=0;j<=5;j++){//left->right
-                        if((inputColor==color[i][j]||color[i][j]== White) &&Record[i][j]!=Max[i][j]+1){
-                            if(i==4&&j==5){
-                                if(color[i-1][j]!= bblue || color[i][j-1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }
-                            }else if(i==4&&j==0){
-                                if(color[i-1][j]!= bblue||color[i][j+1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }
-                            }else if(i==0&&j==5){
-                                if(color[i+1][j]!= bblue || color[i][j-1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }
-                            }else if(i==0&&j==0){
-                                if(color[i+1][j]!= bblue ||color[i][j+1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }
-                            }else if(j==5){
-                                if(color[i-1][j]!= bblue|| color[i+1][j]!= bblue || color[i][j-1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }
-                            }else if(j==0){
-                                if(color[i-1][j]!= bblue|| color[i+1][j]!= bblue||color[i][j+1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }
-                            }else if(i==4){
-                                if(color[i-1][j]!= bblue|| color[i][j-1]!= bblue ||color[i][j+1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }
-                            }else if(i==0){
-                                if(color[i+1][j]!= bblue || color[i][j-1]!= bblue ||color[i][j+1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }
-                            }else{
-                                if(color[i-1][j]!= bblue|| color[i+1][j]!= bblue || color[i][j-1]!= bblue ||color[i][j+1]!= bblue){
-                                    x=i;
-                                    y=j;
-                                }else{
-                                    x=i;
-                                    y=j;
-                                }
-                            }
-
-                        }
+    /////////////////////////////////////////////////////////////////////////////////
+            if((inputColor==color[0][5]||color[0][5]== White) &&Record[0][5]== 0){
+                x=0;
+                y=5;
+                return;
+            }
+            if((inputColor==color[0][0]||color[0][0]== White) &&Record[0][0]== 0){
+                x=0;
+                y=0;
+                return;
+            }
+             if((inputColor==color[4][0]||color[4][0]== White) &&Record[4][0]== 0){
+                x=4;
+                y=0;
+                return;
+            }
+             if((inputColor==color[4][5]||color[4][5]== White) &&Record[4][5]== 0){
+                x=4;
+                y=5;
+                return;
+            }
+    //////////////////////////////////////////////////////////////////////////////////2
+            for(int i=4;i>=0;i--){//up
+                for(int j=0;j<=5;j++){//left->right
+                    if(color[i][j] == player1)continue;
+                    if(Max[i][j]==Record[i][j]+1){
+                         if((i<4 && color[i+1][j] ==player1) || (i>0 && color[i-1][j]==player1) || (j>0 && color[i][j-1]==player1) || ((j<5 && color[i][j+1]==player1))){
+                            x=i;
+                            y=j;
+                            return;
+                         }
                     }
                 }
             }
-////////////////////////////////////////////////////////////////////////////////////////
-            int k=4;
+ ////
+           /* int k=4;
             for(int j=0;j<=5;j++){//right ->left
                 if((inputColor==color[k][j]||color[k][j]== White) &&Record[k][j]!=Max[k][j]+1){
                     x=k;
                     y=j;
+
                 }
                 k--;
-            }
+
+            }*/
+
             for(int i=4;i>=0;i--){
                 if((inputColor==color[i][i]||color[i][i]== White) &&Record[i][i]!=Max[i][i]+1){
                     x=i;
                     y=i;
+                    return;
+
                 }
             }
-////////////////////////////////////////////////////////////////////////////////////
-            for(int j=5;j>=0;j--){
+ ////
+///////////////////////////////////////////////////////////////////////////3(Record[4][j]==1 || color[4][j] ==  player1)
+            for(int i=0;i<=4;i++){//up
+                for(int j=0;j<=5;j++){//left->right
+                    if((Record[4][j]==1 || color[4][j] ==  player1)||(Record[0][j]==1 || color[0][j] == player1) || (Record[i][0]==2 || color[i][0] ==  player1) ||(Record[i][5]==2 || color[i][5] ==  player1)){
+                        continue;
+                    }else if(color[i][j] == player1 || color[i][j] == Black){
+                        continue;
+                    }else{
+                        x=i;
+                        y=j;
+                        return;
+                    }
+
+                    }
+            }
+////////////////////////////////////////////////////////////////////////////4
+            for(int i=0;i<=4;i++){//up
+                    for(int j=0;j<=5;j++){//left->right
+                        if(inputColor==color[i][j]||color[i][j]== White){
+                            if(Max[i][j]!=Record[i][j]+1){
+                                if((i<4 && color[i+1][j] ==player1) || (i>0 && color[i-1][j]==player1) || (j>0 && color[i][j-1]==player1) || ((j<5 && color[i][j+1]==player1))){
+                                    x=i;
+                                    y=j;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+            }
+///////////////////////////////////////////////////////5
+            for(int i=4;i>=0;i--){//up
+                    for(int j=5;j>=0;j--){//left->right
+                        if(inputColor==color[i][j]||color[i][j]== White){
+                            x=i;
+                            y=j;
+                            return;
+                        }
+                    }
+            }
+            return;
+///////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////¹ï¨¤
+        /*    int k=4;
+            for(int j=0;j<=5;j++){//right ->left
+                if((inputColor==color[k][j]||color[k][j]== White) &&Record[k][j]!=Max[k][j]+1){
+                    x=k;
+                    y=j;
+
+                }
+                k--;
+                return;
+            }
+
+            for(int i=4;i>=0;i--){
+                if((inputColor==color[i][i]||color[i][i]== White) &&Record[i][i]!=Max[i][i]+1){
+                    x=i;
+                    y=i;
+                    return;
+                }
+            }*/
+////////////////////////////////////////////////////////////////////////////////////Àð
+            /*for(int j=5;j>=0;j--){
                 if((inputColor==color[4][j]||color[4][j]== White) &&Record[4][j]!=Max[4][j]+1){
                     x=4;
                     y=j;
@@ -107,52 +150,9 @@
                     x=0;
                     y=j;
                 }
-            }
-/////////////////////////////////////////////////////////////////////////////////
-      /*  if((inputColor==color[4][5]||color[4][5]== White) &&Record[4][5]== 0){
-            x=4;
-            y=5;
-        }
-         if((inputColor==color[0][5]||color[0][5]== White) &&Record[0][5]== 0){
-            x=0;
-            y=5;
-        }
-         if((inputColor==color[4][0]||color[4][0]== White) &&Record[4][0]== 0){
-            x=4;
-            y=0;
-        }
-         if((inputColor==color[0][0]||color[0][0]== White) &&Record[0][0]== 0){
-            x=0;
-            y=0;
-        }*/
-        for(int i=4;i>=0;i--){//up
-            for(int j=0;j<=5;j++){
-                if((inputColor==color[i][j]||color[i][j]== White) &&Record[i][j]==Max[i][j]){
-                    x=i;
-                    y=j;
-                }
-            }
-        }
-    for(int j=5;j>=0;j--){
-        if((inputColor==color[0][j]||color[0][j]== White) &&Record[0][j]!=Max[0][j]+1){
-            if(color[1][j]!= bblue){
-                x=0;
-                y=j;
-            }else{
-                x=0;
-                y=j;
-            }
-        }
-        if((inputColor==color[4][j]||color[4][j]== White) &&Record[4][j]!=Max[4][j]+1){
-            if(color[3][j]!= bblue){
-                x=4;
-                y=j;
-            }else{
-                x=4;
-                y=j;
-            }
-        }
-    }
+            }*/
+
+
 
         }
         // Any Code You Want to Add
